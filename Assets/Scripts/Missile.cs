3f28be92517d56edaf496eb4f49717c74a97ff1e
Jack the Ripper <andreastheripper@aol.de>
2017-02-20 17:53:35 +0100

    missiles use proportional navigation as guidance algorithm

--- Assets/Scripts/Missile.cs
+++ Assets/Scripts/Missile.cs
@@ -3,9 +3,11 @@
 [RequireComponent(typeof(Rigidbody))]
 public class Missile : Projectile
 {
-    public float MaxRotationDelta = 10f;
+    public float Proportional = 4f;
+    public float MaxAcceleration = 50;
+    public float MaxLockAngle = 70f;
     public Rigidbody Target;
-    
+
     protected virtual void FixedUpdate()
     {
         if (Target == null)
@@ -13,15 +15,31 @@ public class Missile : Projectile
             return;
         }
 
-        var rigidBody = GetComponent<Rigidbody>();
-        var aimpoint = Target.velocity*Time.fixedDeltaTime + Target.position;
-        var aimPointDirection = Quaternion.LookRotation(aimpoint - rigidBody.position);
-        var newDirection = Quaternion.RotateTowards(
-            rigidBody.rotation,
-            aimPointDirection,
-            MaxRotationDelta);
+        // https://en.wikipedia.org/wiki/Proportional_navigation
+        var missile = GetComponent<Rigidbody>();
+        var range = Target.position - missile.position;
 
-        rigidBody.velocity = newDirection*Vector3.forward*Speed;
-        rigidBody.rotation = newDirection;
+        var lineOfSightAngle = Vector3.Angle(missile.velocity.normalized, range.normalized);
+
+        if (lineOfSightAngle < -MaxLockAngle ||
+            lineOfSightAngle > MaxLockAngle)
+        {
+            Target = null;
+            return;
+        }
+
+        var relativeVelocity = Target.velocity - missile.velocity;
+        var rotationVector = Vector3.Cross(range, relativeVelocity)/Vector3.Dot(range, range);
+        var acceleration = -Proportional * Vector3.Cross(
+                               relativeVelocity.magnitude * missile.velocity.normalized,
+                               rotationVector);
+
+        acceleration = new Vector3(
+            Mathf.Clamp(acceleration.x, -MaxAcceleration, MaxAcceleration),
+            Mathf.Clamp(acceleration.y, -MaxAcceleration, MaxAcceleration),
+            Mathf.Clamp(acceleration.z, -MaxAcceleration, MaxAcceleration));
+
+        missile.AddForce(acceleration * missile.mass);
+        missile.rotation = Quaternion.LookRotation(missile.velocity.normalized);
     }
 }
