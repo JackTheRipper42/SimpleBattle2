40a1f7b17652d03bcc90038e2b0593692b86de70
Jack the Ripper <andreastheripper@aol.de>
2017-02-28 21:33:36 +0100

    DummyFlight script can follow waypoints without using real physics

--- Assets/Scripts/DummyFlight.cs
+++ Assets/Scripts/DummyFlight.cs
@@ -4,11 +4,44 @@
 public class DummyFlight : MonoBehaviour
 {
     public float Velocity = 2f;
-    public float AngularVelocity = 1f;
+    public float MaxAngleChange = 5f;
+    public Transform[] Waypoints;
 
-    protected virtual void Update()
+    private int _waypointIndex;
+
+    protected virtual void Start()
     {
-        GetComponent<Rigidbody>().velocity = transform.forward * Velocity;
-        GetComponent<Rigidbody>().angularVelocity = transform.up * AngularVelocity;
+        //GetComponent<Rigidbody>().velocity = transform.forward * Velocity;
+        _waypointIndex = 0;
+    }
+
+    protected virtual void FixedUpdate()
+    {
+        if (_waypointIndex >= Waypoints.Length)
+        {
+            return;
+        }
+
+        var nextWayPoint = Waypoints[_waypointIndex];
+
+        var deltaX = nextWayPoint.position - Rigidbody.position;
+
+        if (deltaX.magnitude < 0.1f)
+        {
+            _waypointIndex++;
+            return;
+        }
+
+        var currentRotation = Rigidbody.rotation;
+        var desiredRotation = Quaternion.LookRotation(deltaX.normalized);
+        var newRotation = Quaternion.RotateTowards(currentRotation, desiredRotation, MaxAngleChange);
+
+        Rigidbody.velocity = newRotation*Vector3.forward*Velocity;
+        Rigidbody.rotation = newRotation;
+    }
+
+    protected Rigidbody Rigidbody
+    {
+        get { return GetComponent<Rigidbody>(); }
     }
 }
